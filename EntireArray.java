2095110d802b958e09b7514575a030abebeba5d4
Anamika <89299164+202Anamika@users.noreply.github.com>
2023-06-28 21:24:54 +0530

    Add files via upload

---
+++ EntireArray.java
@@ -0,0 +1,16 @@
+public class EntireArray {
+        public static void main(String[] args)
+        {
+            int[] arr = { -100, -75, 55, 100, 40, 30, 20, 25, 12 };
+
+            System.out.print("Elements of given array are: ");
+            for (int i = 0; i < arr.length; i++)
+            {
+                System.out.print(arr[i] + " ");
+            }
+        }
+    }
+
+
+
+
