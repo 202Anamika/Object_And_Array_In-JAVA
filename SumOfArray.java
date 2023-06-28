2929bb08e416261b7510e50bedfe50478a76c13b
Anamika <89299164+202Anamika@users.noreply.github.com>
2023-06-28 21:54:13 +0530

    Add files via upload

---
+++ SumOfArray.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class SumOfArray {
+    public static void main(String[] args)
+    {
+        int arr[]=new int[51];
+        int sum=0;
+        Scanner r=new Scanner(System.in);
+
+        System.out.print("Enter Elements of Array ");
+        for(int i=0;i<arr.length;i++)
+        {
+        arr[i]=r.nextInt();
+        }
+        System.out.println("Array Elements ");
+
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i]+" ");
+            sum=arr[i]+sum;
+        }
+        System.out.print("Addition of Elements "+sum);
+    }
+}
